df5b76559c7684a5007502085e9f7089bbc33ae1
Eddie Wong <isp396@gmail.com>
2013-06-25 12:18:39 +0800

    Add bootstrap

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
 	<head>
-		<title>Check My Battery Level</title>
+		<title>FFOS HTML5 Bootstrap Test</title>
 		<meta name="viewport" content="width=device-width" />
-		<script src="js/jquery-1.9.1.min.js"></script>
-		<script src="js/app.js"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" language="javascript" charset="utf-8" src="js/bootstrap.min.js"></script>
+		
+		<link rel="shortcut icon" href="http://teamtreehouse.com/assets/favicon.ico">
+		<link rel="icon" href="http://teamtreehouse.com/assets/favicon.ico">
+		<link rel="stylesheet" type="text/css" media="all" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" media="all" href="css/bootstrap-responsive.min.css">
+		<link rel="stylesheet" type="text/css" media="all" href="css/global.css">
 	</head>
 	
 	<body>
-		<button id="get-battery">Get Battery Level</button>
-		<p>How much juice is left? <span id="battery-pct"></span></p>
+		
 	</body>
 </html>
